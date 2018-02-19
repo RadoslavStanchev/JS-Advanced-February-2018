4867a99b0db619146374f29622cfcc121a625d3a
RadoslavStanchev <draconissickle@gmail.com>
2018-02-19 14:43:02 +0200

    tryng to add firsst homework

---
+++ 01.Document Object Model/random shit.js
@@ -0,0 +1,10 @@
+function solution(){
+
+    let kelvin = 294
+    let celsius = 294 - 273
+//finding fahrenheit's value
+    let fahrenheit = celsius * (9/5) + 32
+    console.log(`The temperature is ${fahrenheit.floor()} degrees fahrenheit.`)
+    const kelvinMessage = prompt('What is the Kelvin temperature today?')
+}
+
