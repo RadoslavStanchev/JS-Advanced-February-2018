6e69874faf13558925b6508773809a8a8b76b56a
RadoslavStanchev <draconissickle@gmail.com>
2018-02-22 10:04:58 +0200

    Added 4th lecture : jQuery Lab

---
+++ 04.jQuery/05.Selectable Towns/select-towns.js
@@ -0,0 +1,15 @@
+function attachEvents(){
+    $('#items').find('li').on('click', function (){
+        if(!$(this).attr('data-selected')){
+            $(this).attr('data-selected', true)
+            $(this).css('background-color', '#DDD')
+        } else {
+            $(this).removeAttr('data-selected')
+            $(this).css('background-color', '')
+        }
+    })
+    $('#showTownsButton').on('click', function () {
+        let result = $('#items > li[data-selected]').toArray().map(li => li.textContent).join(', ')
+        $('#selectedTowns').text('Selected towns: ' + result)
+    })
+}
