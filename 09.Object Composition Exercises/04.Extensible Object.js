485b3fe872497446431927d724666ef6e2f019aa
RadoslavStanchev <draconissickle@gmail.com>
2018-03-01 11:24:41 +0200

    Added Object Composition Exercises

---
+++ 09.Object Composition Exercises/04.Extensible Object.js
@@ -0,0 +1,24 @@
+function getExtensibleObject (){
+    let obj = {
+        extend: (template) => {
+        for (let property in template) {
+            if(typeof template[property] === 'function'){
+                Object.getPrototypeOf(obj)[property] = template[property]
+            } else {
+                obj[property] = template[property]
+            }
+        }
+    }
+}
+
+    return obj
+}
+let extensibleObject = getExtensibleObject()
+extensibleObject.extend({
+        extensionMethod: function () {
+            return 'neshto'
+        },
+        extensionProperty: 'someString'
+
+})
+console.log(extensibleObject)
---
+++ 09.Object Composition Exercises/07.DOM Highlighter/dom-highlighter.js
@@ -0,0 +1,28 @@
+function traverseDom(selector){
+    let targetNode = $(selector)
+    let maxDepth = 0
+    let deepestNode = targetNode
+
+    depthFirstSearch(0, targetNode)
+    heightFromBottomToTargetNode(maxDepth, deepestNode)
+
+    function heightFromBottomToTargetNode(remainingNodes, currentNode){
+        if(remainingNodes === -1){
+            return
+        }
+        currentNode.addClass('highlight')
+        let parent = currentNode.parent()
+        heightFromBottomToTargetNode(remainingNodes - 1, $(parent))
+    }
+    function depthFirstSearch(depth, currentNode){
+        if(depth > maxDepth){
+            maxDepth = depth
+            deepestNode = currentNode
+        }
+        let children = currentNode.children()
+        for (let child of children) {
+            depthFirstSearch(depth + 1, $(child))
+        }
+    }
+}
+traverseDom()
