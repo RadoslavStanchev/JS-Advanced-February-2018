befc0dcf3868ce3f5dd9b7a75a29e3a546a2354f
RadoslavStanchev <draconissickle@gmail.com>
2018-03-09 10:32:19 +0200

    Added Unit Testing Exercises

---
+++ 11.Unit Testing Exercises/unit testing/02.Even or odd unit tests.js
@@ -0,0 +1,31 @@
+let expect = require('chai').expect;
+
+describe('isOddOrEven',function () {
+    it('should return odd on {"а"}', () => {
+        expect(isOddOrEven("а")).to.equal('odd');
+    });
+
+    it('should return even on {"аb"}', () => {
+        expect(isOddOrEven("аb")).to.equal('even');
+    });
+
+    it('should return even on {""}', () => {
+        expect(isOddOrEven("")).to.equal('even');
+    });
+
+    it('should return even on {"a" * 100}', () => {
+        expect(isOddOrEven('a'.repeat(100))).to.equal('even');
+    });
+
+    it('should return odd on {"a" * 999}', () => {
+        expect(isOddOrEven('a'.repeat(999))).to.equal('odd');
+    });
+
+    it('should return undefined on {new Date}', () => {
+        expect(isOddOrEven(new Date())).to.be.undefined;
+    });
+
+    it('should return undefined on {1}', () => {
+        expect(isOddOrEven(1)).to.be.undefined;
+    });
+});
