f0434ee3d8db5316b409b534b7ec5670acd4f212
RadoslavStanchev <draconissickle@gmail.com>
2018-03-09 10:24:40 +0200

    Added Unit Testing Lab Exercises

---
+++ 10.Unit Testing/test/example test.js
@@ -0,0 +1,14 @@
+let expect = require('chai').expect
+
+function sum(arr) {
+    let sum = 0
+    for (num of arr)
+        sum += Number(num)
+    return sum
+}
+
+describe('Group', function () {
+    it('Name of the test', function () {
+        expect().to.be.equal()
+    })
+})
